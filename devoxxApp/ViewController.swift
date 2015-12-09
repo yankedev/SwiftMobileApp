078b9f992a7c2a71b6d5f6371507b77ff3e2aac6
maxday <got2bex@gmail.com>
2015-12-09 20:58:42 +0100

    Initial Commit

---
+++ devoxxApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devoxxApp
+//
+//  Created by maxday on 09.12.15.
+//  Copyright (c) 2015 maximedavid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
