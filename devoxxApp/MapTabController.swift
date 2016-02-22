a00a2d9b14b354967e59fe28224b65d6def6232f
maxday <got2bex@gmail.com>
2016-02-22 02:38:14 +0100

    AL map

--- devoxxApp/MapTabController.swift
+++ devoxxApp/MapTabController.swift
@@ -28,26 +28,25 @@ public class MapTabController : UIViewController {
     override public func viewDidLoad() {
         
         
-        
-        seg = UISegmentedControl(frame: CGRectMake(40,80,300,25))
-
+        seg = UISegmentedControl()
+        seg.translatesAutoresizingMaskIntoConstraints = false
         seg.insertSegmentWithTitle("Venue map", atIndex: 0, animated: false)
+        
         
         setupSegments()
         
         seg.addTarget(self, action: Selector("change:"), forControlEvents: .ValueChanged)
         
         seg.tintColor = ColorManager.topNavigationBarColor
-        seg.selectedSegmentIndex = 0
         
         
-        accessView = UIView(frame: CGRectMake(0,120,380,600))
+        
+        accessView = UIView()
+        accessView.translatesAutoresizingMaskIntoConstraints = false
         
         currentView?.removeFromSuperview()
         let controller = MapController()
-        controller.view.frame = CGRectMake(0, 0, accessView.frame.width, accessView.frame.height)
-        currentView = controller.view
-        controller.reset()
+        currentView = UIView()
         accessView.addSubview(currentView!)
         
         self.view.addSubview(seg)
@@ -63,18 +62,47 @@ public class MapTabController : UIViewController {
         
         
         
+        
+        
+        
+        let viewDictionary = ["seg":seg, "accessView":accessView]
+        let layout = NSLayoutFormatOptions(rawValue: 0)
+        
+        let horizontalContraint0:[NSLayoutConstraint] = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[seg]-10-|", options: layout, metrics: nil, views: viewDictionary)
+        
+        let horizontalContraint1:[NSLayoutConstraint] = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[accessView]-0-|", options: layout, metrics: nil, views: viewDictionary)
+        
+        let paddingTop = (self.navigationController?.navigationBar.frame.size.height)! + 10
+        
+        
+        let verticalContraint0:[NSLayoutConstraint] = NSLayoutConstraint.constraintsWithVisualFormat("V:|-\(paddingTop)-[seg(20)]-10-[accessView]-0-|", options: layout, metrics: nil, views: viewDictionary)
+        
+        
+        view.addConstraints(horizontalContraint0)
+        view.addConstraints(horizontalContraint1)
+        view.addConstraints(verticalContraint0)
+        
+        
+        seg.selectedSegmentIndex = 0
+        
+        view.layoutIfNeeded()
+        
+        
+        segZero()
+
+        
+        
+        
+        
+        
+        
     }
     
     func change(sender : UISegmentedControl) {
         print("changed")
         
         if(sender.selectedSegmentIndex == 0) {
-            currentView?.removeFromSuperview()
-            let controller = MapController()
-            controller.view.frame = CGRectMake(0, 0, accessView.frame.width, accessView.frame.height)
-            currentView = controller.view
-            controller.reset()
-            accessView.addSubview(currentView!)
+            segZero()
         }
         
         if(sender.selectedSegmentIndex > 0) {
@@ -96,4 +124,12 @@ public class MapTabController : UIViewController {
         self.parentViewController?.parentViewController?.removeFromParentViewController()
     }
     
+    func segZero() {
+        currentView?.removeFromSuperview()
+        let controller = MapController()
+        controller.view.frame = CGRectMake(0, 0, accessView.frame.width, accessView.frame.height)
+        currentView = controller.view
+        controller.reset()
+        accessView.addSubview(currentView!)
+    }
 }
