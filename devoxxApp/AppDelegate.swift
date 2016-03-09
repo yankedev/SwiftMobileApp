417769c1099a16aed7a8072591ad1371feb9a5a0
Maxime DAVID <contact@maximedavid.fr>
2016-03-09 22:04:46 +0100

    rename completed

--- devoxxApp/AppDelegate.swift
+++ devoxxApp/AppDelegate.swift
@@ -86,7 +86,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("devoxxApp", withExtension: "momd")!
+        let modelURL = NSBundle.mainBundle().URLForResource("My_Devoxx", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
 
