68af1bdae7a9127504deca81569d3b6b3252e229
Maxime DAVID <contact@maximedavid.fr>
2016-04-01 01:44:20 +0200

    storyboard

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -64,10 +64,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     */
     
-    func application(application: UIApplication, supportedInterfaceOrientationsForWindow window: UIWindow?) -> UIInterfaceOrientationMask {
+   /* func application(application: UIApplication, supportedInterfaceOrientationsForWindow window: UIWindow?) -> UIInterfaceOrientationMask {
         return [.Portrait, .PortraitUpsideDown]
     }
-    
+    */
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
